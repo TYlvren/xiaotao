483f12bc150c8ec95977934a58a48c4c32dad914
TYlvren <649729063@qq.com>
2019-04-10 11:51:58 +0800

    Initial commit

---
+++ src/main/java/com/cskaoyan/distributionnews/DistributionnewsApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.distributionnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionnewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionnewsApplication.class, args);
+    }
+
+}
